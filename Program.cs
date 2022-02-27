933a87806b0617176102907d017abffe7e5af00f
SajeevDarshanapu <67383521+SajeevDarshanapu@users.noreply.github.com>
2022-02-27 15:08:04 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿int n1, n2, sum;
+Console.WriteLine("Enter your first number:");
+n1=int.Parse(Console.ReadLine());
+Console.WriteLine("Enter your second number:");
+n2=int.Parse(Console.ReadLine());   
+
+sum=n1 + n2;
+Console.WriteLine(sum);
